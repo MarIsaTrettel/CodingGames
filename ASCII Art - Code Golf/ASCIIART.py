8d9b0a22217f479d0daddd19d02ea904957922cf
MarIsaTrettel <mariaisabeltrettel@gmail.com>
2024-05-03 21:16:16 -0300

    rules and input

---
+++ ASCII Art - Code Golf/ASCIIART.py
@@ -0,0 +1,40 @@
+
+""" 
+ASCII art allows you to represent forms by using characters. To be precise, in our case, these forms are words. 
+For example, the word "MANHATTAN" could be displayed as follows in ASCII art:
+
+ 
+# #  #  ### # #  #  ### ###  #  ###
+### # # # # # # # #  #   #  # # # #
+### ### # # ### ###  #   #  ### # #
+# # # # # # # # # #  #   #  # # # #
+# # # # # # # # # #  #   #  # # # #
+ 
+​Your mission is to write a program that can display a line of text in ASCII art in a style you are given as input.
+"""
+""" 
+Input
+Line 1: the width L of a letter represented in ASCII art. All letters are the same width.
+
+Line 2: the height H of a letter represented in ASCII art. All letters are the same height.
+
+Line 3: The line of text T, composed of N ASCII characters 
+"""
+
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l = int(input())
+h = int(input())
+t = input()
+for i in range(h):
+    row = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("answer")
